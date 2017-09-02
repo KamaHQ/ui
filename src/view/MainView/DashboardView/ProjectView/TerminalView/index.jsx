273810df960cdd400b935c269b185d09b365dfdf
Sergei Egorov <sergei@uvita.eu>
2017-09-02 16:51:48 +0200

    Expose logs as SSE stream (Closes #16)

--- src/view/MainView/DashboardView/ProjectView/TerminalView/index.jsx
+++ src/view/MainView/DashboardView/ProjectView/TerminalView/index.jsx
@@ -6,6 +6,8 @@ import React from "react";
 import Rx from "rxjs";
 import Terminal from "xterm";
 
+import EventSourceObservable from "../../../../../utils/EventSourceObservable";
+
 export default class TerminalView extends React.Component {
 
     term = new Terminal({
@@ -45,42 +47,19 @@ export default class TerminalView extends React.Component {
             .filter(it => it)
             .switchMap(number => this.props$.map(({ params: { organization, project } }) => `${organization}/${project}`).distinctUntilChanged().switchMap(key => Rx.Observable.defer(() => {
                 this.term.clear();
-                let lastHit = undefined;
-                return Rx.Observable
-                    .defer(() => Rx.Observable
-                        .ajax({ method: "POST", url: "/plugins/logging-elasticsearch/logz", body: JSON.stringify({
-                            size: 100,
-                            sort: [
-                                { timestamp: "asc" },
-                                { _uid: "asc" },
-                            ],
-                            search_after: lastHit ? lastHit.sort : [0, null],
-                            query: {
-                                bool: {
-                                    must: [
-                                        { term: { number } },
-                                        { term: { key } }
-                                    ]
-                                }
-                            }
-                        })})
-                    )
-                    .pluck("response", "hits", "hits")
-                    .catch(() => Rx.Observable.empty())
-                    .repeatWhen(it => it.delay(1000))
-                    .filter(it => it.length > 0)
-                    .do(items => {
-                        for (const { _source: { message, timestamp } } of items) {
-                            const prefix = "[" + moment(timestamp).format("HH:mm:ss.SSS") + "] ";
-                            if (_.endsWith(message, "\n")) {
-                                this.term.writeln(prefix + message.slice(0, -1));
-                            } else {
-                                this.term.write(prefix + message);
-                            }
-                            this.term.fit();
-                            this.term.scrollToBottom();
+
+                return EventSourceObservable
+                    .create(`/plugins/logging-elasticsearch/${encodeURIComponent(key)}/${number}/logz`)
+                    .do(({ message, timestamp }) => {
+                        const prefix = "[" + moment(timestamp).format("HH:mm:ss.SSS") + "] ";
+                        if (_.endsWith(message, "\n")) {
+                            this.term.writeln(prefix + message.slice(0, -1));
+                        } else {
+                            this.term.write(prefix + message);
                         }
-                        lastHit = _.last(items)
+                        this.term.fit();
+                        // TODO do not scroll if user did manual scrolling aka "follow logs" checkbox
+                        this.term.scrollToBottom();
                     });
             })))
             .subscribe();
